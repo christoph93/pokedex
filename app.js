d6b038d54cbc1711ea32bdf2ebfc03db919a643c
Christoph Califice <christoph.califice@hotmail.com>
2020-09-06 15:01:14 +0200

    test 2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+import App from './src/App';
+
 const http = require('http');
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  
 });
 
 server.listen(port,() => {
